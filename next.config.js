4b313e05a3c9867694e78667625a3013ac8bb849
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2024-06-21 18:56:18 +0800

    test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   webpack: (config) => {
     config.resolve.alias.canvas = false;
-  
+
     return config;
   },
-}
+};
+
+module.exports = nextConfig;
